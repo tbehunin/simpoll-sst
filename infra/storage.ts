255df5daedbb29ff0dd657e459eb0c618ff9bf94
Todd Behunin <tbehunin@gmail.com>
2024-11-13 19:09:21 -0800

    Adding storage

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,1 +1,11 @@
+// Create the DynamoDB table
+export const table = new sst.aws.Dynamo("Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
+  });
+  
+  // Create an S3 bucket
 export const bucket = new sst.aws.Bucket("Uploads");
